072d2328831e73059e60bcab7ffe5ca930033c64
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-23 11:10:53 -0400

    updating auth model WIP

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AuthDocument = HydratedDocument<AuthModel>;
+
+export interface AuthModel extends Base {}
+@Schema({ timestamps: true })
 export class AuthModel {
+	@Prop({ required: true })
 	email: string;
+
+	@Prop({ required: true })
 	passwordHash: string;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
 	controllers: [AuthController],
+	imports: [MongooseModule.forFeature([{ name: AuthModel, schema: AuthSchema }])],
 })
 export class AuthModule {}
