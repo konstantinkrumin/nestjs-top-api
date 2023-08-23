aa1ba21bf2f8a2e6666ce9294c9513081e1a91bc
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-23 08:23:04 -0400

    set up connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
-// import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
 	imports: [
-		// MongooseModule.forRoot('mongo://localhost/test'),
+		MongooseModule.forRoot('mongodb://localhost/test'),
 		ConfigModule.forRoot({ isGlobal: true }),
 		AuthModule,
 		TopPageModule,
