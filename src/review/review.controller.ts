dc6d83d32ccff7f3e8b191869e730d94b1cec146
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-02 20:05:37 -0400

    created decorator example

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -16,6 +16,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { UserEmail } from 'src/decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -37,8 +38,10 @@ export class ReviewController {
 		}
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string) {
+	async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
+		console.log(email);
 		return this.reviewService.findByProductId(productId);
 	}
 }
