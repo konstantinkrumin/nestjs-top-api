af03fbb5cb9e26a876eea94d202309e2ea9ef54d
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-02 20:18:14 -0400

    add auth to the e2e tests

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,8 +15,8 @@ import {
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
-import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
-import { UserEmail } from 'src/decorators/user-email.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { UserEmail } from '../decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
