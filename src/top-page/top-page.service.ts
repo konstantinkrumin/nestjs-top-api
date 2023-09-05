132033f1cf30d2039718e769fcc09f42b452955c
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-04 20:44:08 -0400

    set up text search related service

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -36,6 +36,17 @@ export class TopPageService {
 			.exec();
 	}
 
+	async findByText(text: string) {
+		return this.topPageModel
+			.find({
+				$text: {
+					$search: text,
+					$caseSensitive: false,
+				},
+			})
+			.exec();
+	}
+
 	@UseGuards(JwtAuthGuard)
 	async deleteById(id: string) {
 		return this.topPageModel.findByIdAndDelete(id).exec();
