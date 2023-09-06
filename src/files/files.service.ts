64e4d075fbb21ba7c8ec18b6b18729110162b580
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-09-05 20:41:43 -0400

    set up fn to convert to webp as well set up upload file controller

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { format } from 'date-fns';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import * as sharp from 'sharp';
+
 import { FileElementResponse } from './dto/file-element.response';
+import { MFile } from './dto/mfile.class';
 
 @Injectable()
 export class FilesService {
-	async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+	async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dateFolder}`;
 		await ensureDir(uploadFolder);
@@ -19,5 +22,9 @@ export class FilesService {
 		}
 
 		return res;
+	}
+
+	async convertToWebP(file: Buffer): Promise<Buffer> {
+		return sharp(file).webp().toBuffer();
 	}
 }
